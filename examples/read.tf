b1a6825a554d82d42b9f9439c5b444d1ee227e9f
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-10-05 13:18:01 +0100

    add read database example

--- examples/read.tf
+++ examples/read.tf
@@ -15,8 +15,16 @@ data "planetscale_databases" "all" {
   organization = "gophercorp"
 }
 
+data "planetscale_database" "one"{
+  organization = "gophercorp"
+  database = "express"
+}
 output "all_databases" {
   value = data.planetscale_databases.all.databases  
+}
+
+output "database" {
+  value = data.planetscale_database.one.db
 }
 
 

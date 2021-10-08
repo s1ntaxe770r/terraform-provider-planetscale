1b685632492bfdbf1d75d0ec7cab361e6b95b0b6
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-10-08 22:36:11 +0100

    set access token to sensitve value

--- planetscale/provider.go
+++ planetscale/provider.go
@@ -14,6 +14,7 @@ func Provider() *schema.Provider {
 			"access_token": {
 				Type:        schema.TypeString,
 				Required:    true,
+				Sensitive:   true,
 				DefaultFunc: schema.EnvDefaultFunc("PLANETSCALE_TOKEN", nil),
 			},
 		},
